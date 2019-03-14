05c4a55ef10f9c3504683effec923028251fc4a1
tommylees112 <thomas.lees112@gmail.com>
2019-03-14 16:39:28 +0000

    move notes from readme to common_grid

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # vegetation_health
 predicting vegetation health from precipitation and temperature
+
+Notes about the data:
+- vars_list : all of the variables that we are regridding onto a common grid.
+               They are all from different sources
+    [lst_day, lst_night, lst_mean, lst_mean, evap, baresoil_evap, pet, transp,
+    surface_sm, rootzone_sm, sm, precip, ndvi, evi]
+
+- East Africa is defined here as the area of the original .nc file (spi_spei.nc)
+    lat min - lat max : -4.9750023 15.174995
+    lon min - lon max : 32.524994 48.274994
+    BoundingBox(left, bottom, right, top)
+        (32.524994, -4.9750023, 15.174995, 48.274994)
+
+- Time Range
+    2010-01-01 : 2017-01-01
--- data/common_grid.py
+++ data/common_grid.py
@@ -2,21 +2,6 @@
 Script for putting netcdf (.nc) files onto a common grid. This means that the
  location, timesteps and resolution of the data is now the same. This makes
  working with the data a lot simpler!
-
-NOTES:
-- vars_list : all of the variables that we are regridding onto a common grid.
-               They are all from different sources
-    [lst_day, lst_night, lst_mean, lst_mean, evap, baresoil_evap, pet, transp,
-    surface_sm, rootzone_sm, sm, precip, ndvi, evi]
-
-- East Africa is defined here as the area of the original .nc file (spi_spei.nc)
-    lat min - lat max : -4.9750023 15.174995
-    lon min - lon max : 32.524994 48.274994
-    BoundingBox(left, bottom, right, top)
-        (32.524994, -4.9750023, 15.174995, 48.274994)
-
-- Time Range
-    2010-01-01 : 2017-01-01
 """
 
 import xarray as xr
